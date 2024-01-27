d875aae95c2c4d23d2a7ee26169b810604fa92ec
JV-conseil <contact@jv-conseil.net>
2024-01-27 21:09:28 +0100

    dotfiles

--- SETUP/install.sh
+++ SETUP/install.sh
@@ -28,33 +28,54 @@ _jvcl_::install_homebrew() {
   printf "\nInstalling Homebrew..."
 
   if type brew &>/dev/null; then
-    printf " Homebrew is already installed"
+    printf " Homebrew is already installed\n"
     return
   fi
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  ) || printf " ERROR: curl -fsSL .../Homebrew/install/HEAD/install.sh failed\n"
+
+  (
+    for _file in "${_dotfiles[@]}"; do
+      if [[ "${_file}" =~ "nano" ]]; then continue; fi
+      _file="${HOME}/${_file}"
+      if [[ ! -f "${_file}" ]]; then touch "${_file}"; fi
+      if grep -Fq "brew shellenv" "${_file}"; then continue; fi
+      # shellcheck disable=SC2016
+      printf "\n# Homebrew set PATH, MANPATH, etc...\n%s\n" 'eval "$(/opt/homebrew/bin/brew shellenv)"' | tee -a "${_file}" 1>/dev/null
+    done
+  ) || printf " ERROR: adding brew shellenv failed\n"
 }
 
 _jvcl_::install_bash() {
   printf "\nInstalling Bash..."
 
   if brew info bash &>/dev/null || :; then
-    printf " Bash is already installed %s" "$(brew info bash | head -1)"
+    printf " Bash is already installed %s\n" "$(brew info bash | head -1)"
     return
   fi
 
-  echo "brew install bash"
+  (
+    brew install bash
+  ) || printf " ERROR: brew install bash failed\n"
 
-  if ! grep -F -q "/opt/homebrew/bin/bash" "/etc/shells"; then
-    printf "\nAdding /opt/homebrew/bin/bash to /etc/shells...\n"
-    echo "/opt/homebrew/bin/bash" | sudo tee -a "/etc/shells" >/dev/null
-  fi
+  (
+    if ! grep -Fq "/opt/homebrew/bin/bash" "/etc/shells"; then
+      printf "\n    Adding /opt/homebrew/bin/bash to /etc/shells..."
+      echo "/opt/homebrew/bin/bash" | sudo tee -a "/etc/shells" 1>/dev/null
+    fi
+  ) || printf " ERROR: Editing /etc/shells failed"
 
-  printf "\nSetting default shell to Bash...\n"
-  chsh -s "/opt/homebrew/bin/bash"
+  printf "\n    Setting default shell to Bash..." &&
+    (
+      chsh -s "/opt/homebrew/bin/bash"
+    ) || printf " ERROR: chsh -s /opt/homebrew/bin/bash failed"
 
-  printf "\nSHELL=%s\n" "${SHELL}"
-  bash --version | head -1
+  (
+    printf "\n    SHELL=%s" "${SHELL}" &&
+      bash --version | head -1
+  ) || printf " ERROR: bash --version | head -1 failed"
 }
 
 _jvcl_::install_from_brewfile() {
@@ -63,7 +84,7 @@ _jvcl_::install_from_brewfile() {
   printf "\nInstalling from Brewfile..."
 
   if [[ -f "${brewfile}" ]]; then
-    printf " Brewfile is already installed"
+    printf " Brewfile is already installed\n"
     return
   fi
 
@@ -74,7 +95,7 @@ _jvcl_::install_from_brewfile() {
   (
     curl -fsSL "${_remote}/Brewfile" -o "${brewfile}" &&
       brew bundle install --file="${brewfile}"
-  ) || :
+  ) || printf " ERROR: brew bundle install --file=%s failed\n" "${brewfile}"
 }
 
 _jvcl_::install_terminal_profile() {
@@ -82,32 +103,45 @@ _jvcl_::install_terminal_profile() {
   printf "\nInstalling Ubutu profile for Terminal..."
   (
     curl -fsSL "${_remote}/${_file}" -o "${_local}/${_file}" &&
-      open "${_local}/${_file}" &&
-      printf " In Terminal > Settings > Profiles > Ubuntu\n    1. Set Ubuntu as default profile\n    2. Set font to Menlo Regular 16"
-  ) || :
+      # open -ga "/System/Applications/Utilities/Terminal.app" "${_local}/${_file}" &&
+      open -ga "/System/Applications/Utilities/Terminal.app" "${_local}/${_file}" &&
+      printf " In Terminal > Settings > Profiles > Ubuntu\n    1. Set Ubuntu as default profile\n    2. Set Font to Menlo Regular 16\n"
+  ) || printf " ERROR: downloading %s failed\n" "${_file}"
 }
 
 _jvcl_::diff_vscode_user_settings() {
   local _file="vscode.user.settings.json"
-  printf "\nComparing VS Code User Settings... Local <-> Remote\n"
+  printf "\nComparing VS Code User Settings...          LOCAL                <->                        REMOTE\n"
   (
     curl -fsSL "${_remote}/${_file}" -o "${_local}/${_file}" &&
-      diff -wy --strip-trailing-cr "${HOME}/Library/Application Support/Code/User/settings.json" "${_local}/${_file}"
+      diff -Bwy "${HOME}/Library/Application Support/Code/User/settings.json" "${_local}/${_file}"
   ) || :
 }
 
-_jvcl_::inspect_profile_files() {
+_jvcl_::inspect_install() {
   local _file=""
-  printf "\nInspect Profiles files...\n"
-  for _file in ".bashrc" ".bash_profile" ".nanorc" ".profile" ".zprofile"; do
-    (printf "\n%s\n" "${_file}" && cat "${HOME}/${_file}") || :
+  printf "\nInspect dotfiles...\n"
+  for _file in "${_dotfiles[@]}"; do
+    (
+      cat <<EOF
+
+-------------
+${_file}
+-------------
+$(cat "${HOME}/${_file}")
+
+EOF
+    ) || printf "\nERROR: inspecting %s failed" "${_file}"
   done
+
+  printf "\nInspect PATH...\n\n%s\n" "$(echo "${PATH}" | tr ':' '\n')"
 }
 
 _jvcl_::main() {
   local -i DEBUG=0
-  local _remote="https://raw.githubusercontent.com/JV-conseil/.github/main/SETUP"
+  local -a _dotfiles=(".bashrc" ".bash_profile" ".nanorc" ".profile" ".zprofile")
   local _local="${HOME}/Downloads"
+  local _remote="https://raw.githubusercontent.com/JV-conseil/.github/main/SETUP"
 
   cat <<EOF
 
@@ -128,7 +162,7 @@ EOF
   _jvcl_::install_terminal_profile
   _jvcl_::install_from_brewfile
   _jvcl_::diff_vscode_user_settings
-  _jvcl_::inspect_profile_files
+  _jvcl_::inspect_install
   cat <<EOF
 
 
